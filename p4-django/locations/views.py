4586309c6fa163aa4b4af5a7f7ea7d70704f5a25
Riddles <ridwan.arshad@hotmail.com>
2022-09-07 23:00:40 +0100

    all locations endpoint working

--- p4-django/locations/views.py
+++ p4-django/locations/views.py
@@ -5,3 +5,13 @@ from .models import Location
 from .serializers.common import LocationSerializer
 
 # Create your views here.
+class LocationListView(APIView):
+
+  def get(self, _request):
+
+    locations = Location.objects.all()
+    print('locations -->', locations)
+    serialized_locations = LocationSerializer(locations, many=True)
+    print(serialized_locations)
+    print(serialized_locations.data)
+    return Response(serialized_locations.data, status=status.HTTP_200_OK)
