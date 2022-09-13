3b9e6863d8f436f5c092a061413b8f2d67f5ab05
Riddles <ridwan.arshad@hotmail.com>
2022-09-13 21:35:25 +0100

    profile

--- p4-django/client/src/components/LocationSingle.js
+++ p4-django/client/src/components/LocationSingle.js
@@ -29,6 +29,11 @@ const LocationSingle = () => {
     location: parseInt(locationId),
   })
   const [ owner, setOwner ] = useState([])
+  const [ update, setUpdate ] = useState(false)
+  const [ updateReview, setUpdateReview ] = useState({
+    text: '',
+    location: parseInt(locationId),
+  })
 
   const allLikes = (event, likes) => {
     setLiking(liking + 1)
@@ -42,10 +47,8 @@ const LocationSingle = () => {
       const { data } = await axios.get(`/api/locations/${locationId}`)
       console.log(data)
       setLocations(data)
-      // reviews
       setReviews(data.reviews)
       setLikes(data.likes)
-      // [0].owner.id
       setOwner(data.owner)
       console.log('owner  ---->', data.owner)
       console.log('likes  ---->', data.likes)
@@ -69,32 +72,6 @@ const LocationSingle = () => {
     }
   }
 
-  // ! Delete comment
-  // const submitDelete = async (event) => {
-  //   event.preventDefault()
-  //   try {
-  //     // const { data } = await axios.post(
-  //     //   'http://localhost:4000/login',
-  //     //   loginData
-  //     // )
-  //     const res = await axios.delete(
-  //       `/api/reviews/${locationId.review[0].id}/`
-  //     )
-  //     console.log('res-->', res.data.message)
-  //     setErrors(null)
-  //     const { token } = res.data
-  //     // localStorage.setItem('r42', token)
-  //     // axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-  //     navigate('/')
-
-    
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-
-
 
   const handleAddComment = async (event) => {
     event.preventDefault()
@@ -114,8 +91,25 @@ const LocationSingle = () => {
     }
   }
 
-  const handleReviewChange = async (event) => {
+  const handleReview = async (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value })
+  }
+
+  // Update reviews
+  const submitUpdateReview = async (event) => {
+    // event.preventDefault()
+    try {
+      console.log(getToken())
+      console.log('form data -->', formData)
+      const { data } = await axios.put(`/api/reviews/${event.target.name}/`, updateReview, headers())
+      getData()
+      // setFormData({ text: '', location: '', owner: '' }) 
+      setUpdateReview({ text: '', location: '', owner: '' }) 
+      console.log('update data ----->', data.reviews[0].text)
+    } catch (e) {
+      setErrors(e)
+      console.log(errors)
+    }
   }
 
   // Delete comments
@@ -132,6 +126,11 @@ const LocationSingle = () => {
       setErrors(e)
       console.log(errors)
     }
+  }
+
+  const handleEdit = (event) => {
+    setUpdateReview(true)
+    console.log('setUpdate')
   }
 
   //! Likes
@@ -157,16 +156,16 @@ const LocationSingle = () => {
 
   return (
     <>
-      <Container as="main">
+      <Container as='main'>
         <Row>
           { locations ? 
             <>
               <h1>{locations.name}</h1>
         
-              <Col md="6">
+              <Col md='6'>
                 <img className='w-100' src={locations.location_image_1} alt={locations.name} />
               </Col>
-              <Col md="6">
+              <Col md='6'>
                 <h2>History</h2>
                 <p>{locations.history}</p>
                 <hr />
@@ -175,7 +174,7 @@ const LocationSingle = () => {
                 <>
                   <p>{locations.creatures[0].name}</p>
                   <hr />
-                  <Col md="6">
+                  <Col md='6'>
                     <img className='w-100' src={locations.creatures[0].image1} alt={locations.name} />
                   </Col>
                 </>
@@ -184,89 +183,108 @@ const LocationSingle = () => {
                 <p>{locations.trivia}</p>
                 <hr />
                 {locations.youtube_id && 
-                <Col className="title-media mb-4 justify-content-center mt-4">
-                  <div className="youtube">
+                <Col className='title-media mb-4 justify-content-center mt-4'>
+                  <div className='youtube'>
                     <YoutubeEmbed embedId={locations.youtube_id} />
                   </div>
                 </Col>
                 }
                 <hr />
-                <Link to="/locations" className='btn dark'>Back to all Locations</Link>
+                <Link to='/locations' className='btn dark'>Back to all Locations</Link>
               </Col>
               
               {/* <div className='likes'>
-                <Button className='btn btn-primary' variant="primary" size="lg" onClick={(event) => {
+                <Button className='btn btn-primary' variant='primary' size='lg' onClick={(event) => {
                   console.log({ likes })
                   allLikes(event, likes ) 
                 } }>{locations.likes.length += 1}</Button>
               </div>
               <div className='dislike-button'>
-                <Button className='btn btn-primary' variant="primary" size="lg" onClick={(event) => {
+                <Button className='btn btn-primary' variant='primary' size='lg' onClick={(event) => {
                   (event, likes ) 
                 } }>{locations.dislikes.length += 1}</Button>
               </div> */}
-              
-              <div className="grid grid-cols-3">
-                <div className="col-span-2">
-                  <h3>Reviews</h3>
-                  { reviews.length > 0
-                    ?
-                    reviews.map(review => {
-                      const { id, owner, text } = review
-                      return (                       
-                        <div key={review.id} data-bs-spy="scroll" data-bs-target="#scrollspy1" data-bs-offset="200" className="scrollspy-example">
-                          <section>
-                            <h3 className="text-xl font-semibold pt-5 pb-3">{review.owner.username}</h3>
-                            <p>
-                              {review.text}
-                            </p>                 
-                            <button name={review.id} onClick={handleDelete}> 
-                                  Delete
-                            </button>
-                          </section>
-                        </div>          
-                      )
-                    })
-                    :
-                    <>
-                      { errors ? <h2>Something went wrong.</h2> : <p>Loading</p>}
-                    </>
-                  }
-                </div>
-              </div>
 
-              <form onSubmit={handleAddComment}>
-                <div className="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-                  <div className="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
-                    <label htmlFor="comment" className="sr-only">Your comment</label>
-                    <textarea id="comment" rows="4" className="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" name="text" value={formData.text} maxLength="280" onChange={handleReviewChange} placeholder="Write a comment..." required></textarea>
-                  </div>
-                  {/* <div className="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
-                    <label htmlFor="comment" className="sr-only">Your location</label>
-                    <textarea id="comment" rows="4" className="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" name="location" maxLength="280" onChange={handleReviewChange} placeholder="Type '1' for The Shire, '2' for Bree" required></textarea>
-                  </div>
-                  <div className="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
-                    {/* [0].owner.username 
-                    <label htmlFor="comment" className="sr-only">Here</label>
-                    <textarea id="comment" rows="4" className="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" name="owner" maxLength="280" onChange={handleReviewChange} required></textarea>
 
+              {/* COMMENTS SECTION */}
+              <form onSubmit={handleAddComment} >
+                <div className='grid grid-cols-3'>
+                  <div className='col-span-2'>
+                    <div className={update && owner === reviews.owner ? 'review-display hide' : 'review-display'}>
+                      <h3>Reviews</h3>
+                      { reviews.length > 0
+                        ?
+                        reviews.map(review => {
+                          const { id, owner, text } = review
+                          return (                       
+                            <div key={review.id} data-bs-spy='scroll' data-bs-target='#scrollspy1' data-bs-offset='200' className='scrollspy-example'>
+                              
+                              <section>
+                                <h3 className='text-xl font-semibold pt-5 pb-3'>{review.owner.username}</h3>
+                                <p>
+                                  {review.text}
+                                </p>                 
+                        
+                              
+                                <div className="buttons">
+                      
+                                </div>
+                                <div className='mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600'>
+                                  <div className='py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800'>
+                                    <label htmlFor='comment' className='sr-only'>Your comment</label>
+                                    <textarea id='comment' rows='4' className='px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400' name='text' value={formData.text} maxLength='280' onChange={handleReview} placeholder='Write a comment...' required></textarea>
+                                  </div>
+                                  <div className='flex justify-between items-center py-2 px-3 border-t dark:border-gray-600'>
+                                   
+                                    <button name={review.id} onClick={handleDelete}> 
+                                      Delete
+                                    </button>
+                                    {owner === reviews.owner ? (
+                                      <button name={reviews.id} value={formData.text} onClick={handleEdit}>
+                                        Edit
+                                      </button>
+                                      
+                                    ) : (
+                                      <></> 
+                                    )} 
+                                    {owner === reviews.owner ? (
+                                      <button name={reviews.id} onClick={handleDelete}>
+                                        🗑
+                                      </button>
+                                    ) : (
+                                      <></>
+                                    )}
+                                  </div>
+                                    
+                                
+                                </div>
+                              </section>
+                            </div>          
+                          )
+                        })
+                        :
+                        <>
+                          { errors ? <h2>Something went wrong.</h2> : <p>Loading</p>}
+                        </>
+                      }
+                    </div>
                     
-                  </div> */}
-                  <div className="flex justify-between items-center py-2 px-3 border-t dark:border-gray-600">
-                    <button type="submit" value="Add Comment" required className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
-                            Post Review
-                    </button>
                   </div>
+
+                  {/* <form onSubmit={handleAddComment} > */}
+               
                 </div>
               </form>
+        
+
+         
             </>
             :
-            <h2 className="text-center">
-              Error.
-            </h2>
+            <h2 className='text-center'>
+              { errors ? <h2>Something went wrong.</h2> : <p>Loading</p>}
+            </h2> 
           }
         </Row>
-
       </Container>
     </>
   )
